b8365a974ea329298587f89ee506d101b69e98f5
persiska1 <losdavid1@gmail.com>
2022-08-29 22:35:51 +0300

    v 0.3

--- main.py
+++ main.py
@@ -41,13 +41,41 @@ def add_phone(name, phone):
 
 
 def update_client(name, last_name, new_name, new_surname, new_email):
-    cur.execute('''
-    SELECT id FROM client WHERE first_name=%s AND surname=%s;
-    ''', (name, last_name))
-    res = cur.fetchone()[0]
-    cur.execute('''
-    UPDATE client SET first_name=%s, surname=%s, email=%s WHERE id=%s;
-    ''', (new_name, new_surname, new_email, res))
+    print("Для изменения информации о клиенте, пожалуйста, введите нужную Вам команду.\n "
+          "1 - изменить имя; 2 - изменить фамилию; 3 - изменить e-mail; 4 - изменить номер телефона")
+
+    while True:
+        command_sym = int(input())
+        if command_sym == 1:
+            input_id_for_changing_name = input("Введите id клиента имя которого хотите изменить: ")
+            input_name_for_changing = input("Введите имя для изменения: ")
+            cur.execute("""
+                UPDATE client SET first_name=%s WHERE id=%s;
+                """, (input_name_for_changing, input_id_for_changing_name))
+            break
+        elif command_sym == 2:
+            input_id_for_changing_surname = input("Введите id клиента фамилию которого хотите изменить: ")
+            input_surname_for_changing = input("Введите фамилию для изменения: ")
+            cur.execute("""
+                UPDATE client SET surname=%s WHERE id=%s;
+                """, (input_surname_for_changing, input_id_for_changing_surname))
+            break
+        elif command_sym == 3:
+            input_id_for_changing_email = input("Введите id клиента e-mail которого хотите изменить: ")
+            input_email_for_changing = input("Введите e-mail для изменения: ")
+            cur.execute("""
+                UPDATE client SET email=%s WHERE id=%s;
+                """, (input_email_for_changing, input_id_for_changing_email))
+            break
+        elif command_sym == 4:
+            input_phonenumber_you_wanna_change = input("Введите номер телефона который Вы хотите изменить: ")
+            input_phonenumber_for_changing = input("Введите новый номер телефона, который заменит собой старый: ")
+            cur.execute("""
+                UPDATE phone_number SET phone=%s WHERE phone=%s;
+                """, (input_phonenumber_for_changing, input_phonenumber_you_wanna_change))
+            break
+        else:
+            print("К сожалению, Вы ввели неправильную команду, пожалуйста, повторите ввод")
 
 
 def delete_phone(name, last_name):
@@ -74,14 +102,51 @@ def delete_client(name, last_name):
     ''', (name, last_name))
 
 
-def find_client(name, last_name, e_mail, phone_num):
-    cur.execute('''
-    SELECT first_name, surname, email, pn.phone FROM client c
-    LEFT JOIN phone_number pn ON pn.client_id = c.id
-    WHERE first_name=%s AND surname=%s AND email=%s AND pn.phone=%s;
-    ''', (name, last_name, e_mail, phone_num))
-    res = cur.fetchall()
-    print(res)
+def find_client():
+    '''Поиск клиента по имени'''
+    print("Для поиска информации о клиенте, пожалуйста, введите команду, где:\n "
+          "1 - найти по имени; 2 - найти по фамилии; 3 - найти по e-mail; 4 - найти по номеру телефона")
+    while True:
+        input_command_for_finding = int(input("Введите команду для поиска информации о клиенте: "))
+        if input_command_for_finding == 1:
+            input_name_for_finding = input("Введите имя для поиска информации о клиенте: ")
+            cur.execute("""
+            SELECT id, first_name, surname, email, phone
+            FROM client AS cl
+            LEFT JOIN phone AS p ON p.id_phonenumber = cl.id
+            WHERE first_name=%s
+            """, (input_name_for_finding,))
+            print(cur.fetchall())
+        elif input_command_for_finding == 2:
+            input_surname_for_finding = input("Введите фамилию для поиска информации о клиенте: ")
+            cur.execute("""
+            SELECT id, first_name, surname, email, phone
+            FROM client AS cl
+            LEFT JOIN phone AS p ON p.id_phonenumber = cl.id
+            WHERE surname=%s
+            """, (input_surname_for_finding,))
+            print(cur.fetchall())
+        elif input_command_for_finding == 3:
+            input_email_for_finding = input("Введите email для поиска информации о клиенте: ")
+            cur.execute("""
+            SELECT id, first_name, surname, email, phone
+            FROM client AS cl
+            LEFT JOIN phone AS p ON p.id_phonenumber = cl.id
+            WHERE email=%s
+            """, (input_email_for_finding,))
+            print(cur.fetchall())
+        elif input_command_for_finding == 4:
+            input_phonenumber_for_finding = input("Введите номер телефона для поиска информации о клиенте: ")
+            cur.execute("""
+            SELECT id, first_name, surname, email, phone
+            FROM client AS cl
+            LEFT JOIN phone AS p ON p.id_phonenumber = cl.id
+            WHERE phone=%s
+            """, (input_phonenumber_for_finding,))
+            #return cur.fetchone()[0]
+            print(cur.fetchall())
+        else:
+            print("К сожалению, Вы ввели неправильную команду, пожалуйста, повторите ввод")
 
 
 
